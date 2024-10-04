7c538859d4d8b353b32667abcb0f87978a6fba5e
shubham-shootorder <shubham@shootorder.in>
2024-10-04 13:22:58 +0530

    vaporin new -lp

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.expert = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        customBlack: "#000000",
+        customBlue: "#21285e",
+        laravander: "#4E338E",
+        jasmine: "#F9F158",
+      },
+    },
+  },
+  plugins: [],
+};
